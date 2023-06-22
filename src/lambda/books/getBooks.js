f1f5b1cdc6446f8dc23dd84266c9ed8475bc1351
Abdul Rehman <abdulrehmanashfaq.d@gmail.com>
2023-06-22 16:07:53 +0500

    feat: default sorting

--- src/lambda/books/getBooks.js
+++ src/lambda/books/getBooks.js
@@ -1,15 +1,16 @@
 const { connectToDatabase } = require("../db/db");
 const Books = require("../db/models/bookModel");
 
-exports.handler = async ({ sortBy, sortDirection, page, perPage }) => {
+exports.handler = async ({
+  sortBy = "ratingsCount",
+  sortDirection = "DESC",
+  page,
+  perPage,
+}) => {
   try {
     await connectToDatabase();
 
     let sortOptions = {};
-    if (!sortBy && !sortDirection) {
-      sortBy = "averageRating";
-      sortDirection = "DESC";
-    }
 
     sortOptions[sortBy] = sortDirection === "DESC" ? -1 : 1;
     const skip = (page - 1) * perPage;
